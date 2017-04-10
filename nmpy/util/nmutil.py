8c2536886036b2fadb69fd70b320d8350785de11
s-schneider <simon.schneider@wwu.de>
2017-04-10 14:37:09 +0200

    added rotate_stream

--- nmpy/util/nmutil.py
+++ nmpy/util/nmutil.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import, print_function
-from sipy.util.base import cut2shortest
+from sipy.util.base import cut2shortest, split2stations
 
 
-def rotate_streamlist(streamlist, rotation='NE->RT'):
+def rotate_stream(stream, rotation='NE->RT'):
 
-	for station in streamlist:
+	stationlist = split2stations(stream)
+
+	for station in stationlist:
 		station = cut2shortest(station)
 		station.rotate(rotation)
 
-	return streamlist
+	return stationlist
