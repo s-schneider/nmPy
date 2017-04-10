41f5086c89ebfd538545c99e746d4784cab0cee1
s-schneider <simon.schneider@wwu.de>
2017-04-10 13:55:50 +0200

    reorganizing structure of folders

--- nmpy/util/nmutil.py
+++ nmpy/util/nmutil.py
@@ -1,6 +1,11 @@
 from __future__ import absolute_import, print_function
+from sipy.util.base import cut2shortest
 
-def rotate_streamlist():
 
-	
-	return
+def rotate_streamlist(streamlist, rotation='NE->RT'):
+
+	for station in streamlist:
+		station = cut2shortest(station)
+		station.rotate(rotation)
+
+	return streamlist
