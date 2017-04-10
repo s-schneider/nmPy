f7d0c61b04d35b32616bcde11aff98e8fa63dd1c
s-schneider <simon.schneider@wwu.de>
2017-04-10 15:35:55 +0200

    added function to safe a streamlist

--- nmpy/util/nmfile.py
+++ nmpy/util/nmfile.py
@@ -12,3 +12,25 @@ def read_dat_folder(folder):
 	flist.sort()
 
 	return flist
+
+def safe_streamlist(streamlist, format='AH'):
+
+	for station in streamlist:
+		time  = station[0].stats.starttime
+		name    = station[0].stats.station
+		network = station[0].stats.network
+		try:
+			location = station[0].stats.location
+		except:
+			location = ''
+		try:
+			quality =  station[0].stats.mseed['dataquality']
+		except:
+			quality =  ''
+
+		fname = str(time.format_seed()).replace(",",".") + "." + network + "." + name + "." + location + "." + quality + "." + format
+
+		try:
+			station.write(fname, format=format)
+		except:
+			continue
