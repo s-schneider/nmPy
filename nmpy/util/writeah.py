68dd4c13c867e59bcd6fe156cdc2aabe8442090d
s-schneider <simon.schneider@wwu.de>
2017-04-12 17:56:25 +0200

    _write_ah1 writes AH-files from AH-input

--- nmpy/util/writeah.py
+++ nmpy/util/writeah.py
@@ -9,7 +9,7 @@ import numpy as np
 from obspy import Stream, Trace, UTCDateTime
 from obspy.core.util.attribdict import AttribDict
 
-def _write_ah(stream, filename):
+def _write_ah1(stream, filename):
     """
     Reads an AH v1 waveform file and returns a Stream object.
 
@@ -19,33 +19,49 @@ def _write_ah(stream, filename):
     :returns: Stream with Traces specified by given file.
     """
 
-    def _pack_trace(trace, packer):
+    CODESIZE  = 6
+    CHANSIZE  = 6
+    STYPESIZE = 8
+    COMSIZE   = 80
+    TYPEMIN   = 1
+    TYPEMAX   = 6
+    LOGSIZE   = 202
+    LOGENT    = 10
+    NEXTRAS   = 21
+    NOCALPTS  = 30
 
+    def _pack_trace(trace, packer):
 
         # station info
         try:
-            packer.pack_string(tr.stats.station)
-            packer.pack_string(tr.stats.channel)
+            packer.pack_int(CODESIZE)
+            packer.pack_fstring(CODESIZE, tr.stats.station)
+            packer.pack_int(CHANSIZE)
+            packer.pack_fstring(CHANSIZE, tr.stats.channel)
         except:
             print('Input from original AH file')
-            packer.pack_string(tr.stats.ah.station.code)
-            packer.pack_string(tr.stats.ah.station.channel)
+            packer.pack_int(CODESIZE)
+            packer.pack_fstring(CODESIZE, tr.stats.ah.station.code)
+            packer.pack_int(CHANSIZE)
+            packer.pack_fstring(CHANSIZE, tr.stats.ah.station.channel)
 
-        packer.pack_string(tr.stats.ah.station.type)
+        packer.pack_int(STYPESIZE)
+        packer.pack_fstring(STYPESIZE,tr.stats.ah.station.type)
         packer.pack_float(tr.stats.ah.station.latitude)
         packer.pack_float(tr.stats.ah.station.longitude)
         packer.pack_float(tr.stats.ah.station.elevation)
         packer.pack_float(tr.stats.ah.station.gain)
         packer.pack_float(tr.stats.ah.station.normalization)
 
-        #Poles and Zeros
-        packer.pack_float(len(tr.stats.ah.station.poles))
-        packer.pack_float(0)
-        packer.pack_float(len(tr.stats.ah.station.zeros))
-        packer.pack_float(0)
-
         poles = tr.stats.ah.station.poles
         zeros = tr.stats.ah.station.zeros
+
+        #Poles and Zeros
+        packer.pack_float(len(poles))
+        packer.pack_float(0)
+        packer.pack_float(len(zeros))
+        packer.pack_float(0)
+
         for _i in range(1,30):
             try:
                 r, i = poles[_i].real, poles[_i].imag
@@ -80,7 +96,8 @@ def _write_ah(stream, filename):
             packer.pack_int(0)
             packer.pack_float(0)
 
-        packer.pack_string(tr.stats.ah.event.comment)
+        packer.pack_int(COMSIZE)
+        packer.pack_fstring(COMSIZE, tr.stats.ah.event.comment)
 
         # record info
         dtype = tr.stats.ah.record.type
@@ -96,8 +113,10 @@ def _write_ah(stream, filename):
         packer.pack_int(tr.stats.ah.record.start_time.minute)
         packer.pack_float(tr.stats.ah.record.start_time.second)
         packer.pack_float(tr.stats.ah.record.abscissa_min)
-        packer.pack_string(tr.stats.ah.record.comment)
-        packer.pack_string(tr.stats.ah.record.log)
+        packer.pack_int(COMSIZE)
+        packer.pack_fstring(COMSIZE, tr.stats.ah.record.comment)
+        packer.pack_int(LOGSIZE)
+        packer.pack_fstring(LOGSIZE,tr.stats.ah.record.log)
 
         # # extras
         packer.pack_array(tr.stats.ah.extras, packer.pack_float)
@@ -130,10 +149,8 @@ def _write_ah(stream, filename):
         for tr in stream:
             try:
                 ofilename = filename + "." + tr.stats.channel + ".AH"
-                magic = 6
 
                 packer.reset()
-                packer.pack_int(magic)
                 packer = _pack_trace(tr, packer)
 
                 with open(ofilename, 'wb') as fh:
