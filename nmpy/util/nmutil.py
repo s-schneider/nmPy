330477c87c3a5dde6c923029078c341515e474af
s-schneider <simon.schneider@wwu.de>
2017-04-18 14:23:24 +0200

    sorting and merging of traces for rotation improved

--- nmpy/util/nmutil.py
+++ nmpy/util/nmutil.py
@@ -2,18 +2,20 @@ from __future__ import absolute_import, print_function
 from sipy.util.base import cut2shortest, split2stations
 
 
-def rotate_stream(stream, rotation='NE->RT', only_3_components=True, merge=True):
+def rotate_stream(stream, rotation='NE->RT', merge=True):
 	"""
 	Sorts stream by stations and rotates them. If keep_all_components is False, only stations with records of Z, E, N component will be kept
 	"""
-	stationlist = split2stations(stream, merge)
+	stl = split2stations(stream, merge)
 
-	for i, station in enumerate(stationlist):
-		if only_3_components:
-			if len(station) != 3:
-				stationlist.pop(i)
-				continue
-		station = cut2shortest(station)
-		station.rotate(rotation)
 
-	return stationlist
+	for station in stl:
+		try:
+			station.rotate(rotation)
+		except ValueError:
+			cut2shortest(station)
+			station.rotate(rotation)
+
+		station.sort(['channel'])
+
+	return stl
